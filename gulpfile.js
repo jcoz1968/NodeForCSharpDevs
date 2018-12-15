ba8a1a3281e37634956c2747b26ed12790f773c8
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-14 21:10:36 -0600

    getting babel to work with async

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ gulp.task('build', () => {
     return gulp.src('src/**/*.js')
         .pipe(debug())
         .pipe(babel({
-            'presets': ['@babel/preset-env']
+            'presets': ['@babel/preset-env'],
+            'plugins': ['@babel/plugin-transform-runtime']
         }))
         .on('error', (err) => {
             console.log(`Babel error: ${err}`);

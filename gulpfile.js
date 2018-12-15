68695fa88381be940cef9c9b9fe5d8ba07ce3ed9
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-14 20:58:21 -0600

    use gulp to transpile app

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const debug = require('gulp-debug');
+const babel = require('gulp-babel');
+
+gulp.task('build', () => {
+    return gulp.src('src/**/*.js')
+        .pipe(debug())
+        .pipe(babel({
+            'presets': ['@babel/preset-env']
+        }))
+        .on('error', (err) => {
+            console.log(`Babel error: ${err}`);
+        })
+        .pipe(gulp.dest('.compiled'));
+});
+
+gulp.task('default', ['build'], () => {
+    console.log('Run default...');
+});
